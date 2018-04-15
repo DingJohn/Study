6178293ed1d9fed061258684bf21335225d351bd
KevinLIris <chantaldzl@sina.com>
2018-04-15 19:34:54 +0800

    submit stage011_study

--- study/stage011_study.py
+++ study/stage011_study.py
@@ -1,2 +1,59 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+
+print("==============================简单递归计算阶乘=====================")
+
+
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+
+'''
+    ===> fact(5)
+    ===> 5 * fact(4)
+    ===> 5 * (4 * fact(3))
+    ===> 5 * (4 * (3 * fact(2)))
+    ===> 5 * (4 * (3 * (2 * fact(1))))
+    ===> 5 * (4 * (3 * (2 * 1)))
+    ===> 5 * (4 * (3 * 2))
+    ===> 5 * (4 * 6)
+    ===> 5 * 24
+    ===> 120
+'''
+print(fact(5))
+
+print("\n==============================尾递归计算阶乘=====================")
+
+
+def fact1(number):
+    return fact2(number, 1)
+
+
+def fact2(num, defalut):
+    if num == 1:
+        return defalut
+    return fact2(num - 1, num * defalut)
+
+
+print(fact1(5))
+
+print("\n==============================练习(汉诺塔)=====================")
+
+
+def hn(n, a, b, c):
+    if n == 1:
+        return a + " ---> " + c
+    else:
+        hn(n - 1, a, c, b)
+        print(a + " ---> " + b)
+        hn(1, a, b, c)
+        print(a + " ---> " + c)
+        hn(n - 1, b, a, c)
+        print(b + " ---> " + c)
+    return
+
+
+print(hn(2, 'A', 'B', 'C'))
